125f7eaf536920268f8cb91ee7600f3b7893145c
maltebrun <michel.goulas@maltebrun.com>
2015-02-04 19:52:47 +0100

    Ajout - suppression de fichiers + version prod en Lorem Ipsum

---
+++ public/js/utils/dimensions.js
@@ -0,0 +1,8 @@
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+function displayWindowSize() {
+	myWidth = window.innerWidth;
+	myHeight = window.innerHeight;
+	document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
+};
+ 
